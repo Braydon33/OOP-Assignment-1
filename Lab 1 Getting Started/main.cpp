22fd87bc1175b295bd015a12ad1bc292c97d130c
Braydon Duprey <Braydon@braydons-macbook-pro.local>
2016-01-02 18:18:59 -0500

    Initial Commit

---
+++ Lab 1 Getting Started/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 1 Getting Started
+//
+//  Created by Braydon Duprey on 2016-01-02.
+//  Copyright © 2016 Braydon Duprey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
